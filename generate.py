2b267dac5f575774c944839f9bebd92856c79ee3
yuzr <54988669+Mitr-yuzr@users.noreply.github.com>
2022-09-10 12:06:05 +0800

    Base Update

--- generate.py
+++ generate.py
@@ -68,7 +68,8 @@ def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float('Inf')
     return logits
 
 
-def sample_sequence(model, context, length, n_ctx, tokenizer, temperature=1.0, top_k=30, top_p=0.0, repitition_penalty=1.0,
+def sample_sequence(model, context, length, n_ctx, tokenizer, temperature=1.0, top_k=30, top_p=0.0,
+                    repitition_penalty=1.0,
                     device='cpu'):
     context = torch.tensor(context, dtype=torch.long, device=device)
     context = context.unsqueeze(0)
@@ -111,30 +112,28 @@ def fast_sample_sequence(model, context, length, temperature=1.0, top_k=30, top_
 
 
 # 通过命令行参数--fast_pattern，指定模式
-def generate(n_ctx, model, context, length, tokenizer, temperature=1, top_k=0, top_p=0.0, repitition_penalty=1.0, device='cpu',
+def generate(n_ctx, model, context, length, tokenizer, temperature=1, top_k=0, top_p=0.0, repitition_penalty=1.0,
+             device='cpu',
              is_fast_pattern=False):
     if is_fast_pattern:
         return fast_sample_sequence(model, context, length, temperature=temperature, top_k=top_k, top_p=top_p,
                                     device=device)
     else:
-        return sample_sequence(model, context, length, n_ctx, tokenizer=tokenizer, temperature=temperature, top_k=top_k, top_p=top_p,
+        return sample_sequence(model, context, length, n_ctx, tokenizer=tokenizer, temperature=temperature, top_k=top_k,
+                               top_p=top_p,
                                repitition_penalty=repitition_penalty, device=device)
 
 
-def main():
+def startGenerate(prefix, length, nsamples, temperature):
     parser = argparse.ArgumentParser()
     parser.add_argument('--device', default='0,1,2,3', type=str, required=False, help='生成设备')
-    parser.add_argument('--length', default=-1, type=int, required=False, help='生成长度')
     parser.add_argument('--batch_size', default=1, type=int, required=False, help='生成的batch size')
-    parser.add_argument('--nsamples', default=10, type=int, required=False, help='生成几个样本')
-    parser.add_argument('--temperature', default=1, type=float, required=False, help='生成温度')
     parser.add_argument('--topk', default=8, type=int, required=False, help='最高几选一')
     parser.add_argument('--topp', default=0, type=float, required=False, help='最高积累概率')
     parser.add_argument('--model_config', default='config/model_config_small.json', type=str, required=False,
                         help='模型参数')
     parser.add_argument('--tokenizer_path', default='cache/vocab_small.txt', type=str, required=False, help='词表路径')
-    parser.add_argument('--model_path', default='model/final_model', type=str, required=False, help='模型路径')
-    parser.add_argument('--prefix', default='萧炎', type=str, required=False, help='生成文章的开头')
+    parser.add_argument('--model_path', default='model', type=str, required=False, help='模型路径')
     parser.add_argument('--no_wordpiece', action='store_true', help='不做word piece切词')
     parser.add_argument('--segment', action='store_true', help='中文以词为单位')
     parser.add_argument('--fast_pattern', action='store_true', help='采用更加快的方式生成文本')
@@ -143,7 +142,7 @@ def main():
     parser.add_argument('--repetition_penalty', default=1.0, type=float, required=False)
 
     args = parser.parse_args()
-    print('args:\n' + args.__repr__())
+    # print('args:\n' + args.__repr__())
 
     if args.segment:
         from tokenizations import tokenization_bert_word_level as tokenization_bert
@@ -151,10 +150,7 @@ def main():
         from tokenizations import tokenization_bert
 
     os.environ["CUDA_VISIBLE_DEVICES"] = args.device  # 此处设置程序使用哪些显卡
-    length = args.length
     batch_size = args.batch_size
-    nsamples = args.nsamples
-    temperature = args.temperature
     topk = args.topk
     topp = args.topp
     repetition_penalty = args.repetition_penalty
@@ -175,7 +171,7 @@ def main():
             os.makedirs(args.save_samples_path)
         samples_file = open(args.save_samples_path + '/samples.txt', 'w', encoding='utf8')
     while True:
-        raw_text = args.prefix
+        raw_text = prefix
         context_tokens = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(raw_text))
         generated = 0
         for _ in range(nsamples // batch_size):
@@ -216,7 +212,3 @@ def main():
             if args.save_samples:
                 samples_file.close()
             break
-
-
-if __name__ == '__main__':
-    main()
